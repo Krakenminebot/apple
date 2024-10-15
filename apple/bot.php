244a96cef1fee2c5569fa9efda26d333db8d830f
Krakenminebot <softenone@gmail.com>
2024-10-15 07:20:00 +0100

    Update bot.php

--- apple/bot.php
+++ apple/bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "8127379919:AAFm7wx3LBJ6C91Q3SMEJXU9SvcdS1UNkp8";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
